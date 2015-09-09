d4d14ac96f22e370ef578629b120dedcf6e16f12
Alex Corretge <alex@corretge.cat>
2015-09-09 19:09:44 +0200

    CORE-1 doing idempotence to vagrant reload --provision

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,6 +2,20 @@
 
 mkdir -p /etc/puppet/modules
 
-puppet module install puppetlabs-apache
-puppet module install puppetlabs-mysql
-puppet module install nodes/php
+if [ -d "/etc/puppet/modules/apache" ]; then
+    puppet module upgrade puppetlabs-apache
+else
+    puppet module install puppetlabs-apache
+fi
+
+if [ -d "/etc/puppet/modules/mysql" ]; then
+    puppet module upgrade puppetlabs-mysql
+else
+    puppet module install puppetlabs-mysql
+fi
+
+if [ -d "/etc/puppet/modules/php" ]; then
+    puppet module upgrade nodes/php
+else
+    puppet module install nodes/php
+fi
