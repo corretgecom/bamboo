10e99fe15c397c7d2dc116d278dfd555b7747a4f
Àlex Corretgé <alex@corretge.cat>
2015-10-01 14:07:23 +0200

    CORE-25 testing

--- vagrant/manifests/site.pp
+++ vagrant/manifests/site.pp
@@ -22,7 +22,6 @@ package {
         'vim',
         'htop',
         'php5-cli',
-        'php5-gd',
         'git',
         'cifs-utils',
         'curl',
@@ -31,12 +30,43 @@ package {
     ensure => 'latest'
 }
 
-class { ['php', 'php::extension::mysql', 'php::extension::intl', 'php::extension::redis', 'php::extension::curl', 'php::composer', 'php::composer::auto_update']:
+class { 
+	[
+		'php',
+		'php::extension::mysql',
+		'php::extension::intl',
+		'php::extension::redis',
+		'php::extension::curl',
+		'php::composer',
+		'php::composer::auto_update',
+		'php::extension::gd',
+		'php::extension::xdebug'
+	]:
     before => Exec['composer_config']
 }
 
+php::config {
+	'output_buffering=0':
+	file => '/etc/php5/cli/php.ini',
+	require => Package['php5-cli']
+} 
+
 php::config { 'opcache.enable_cli=1':
     file    => '/etc/php5/cli/conf.d/05-opcache.ini',
+    require   => Package['php5-cli']
+}
+
+php::config {
+	[ 
+	    'xdebug.remote_host=10.10.10.1', 
+	    'xdebug.remote_port=9010', 
+	    'xdebug.remote_enable=1',
+	    'xdebug.remote_handler=dbgp',
+	    'xdebug.remote_log=/tmp/log/xdebug.log',
+	    'xdebug.idekey=netbeans-xdebug'
+	]:
+    file    => '/etc/php5/cli/conf.d/20-xdebug.ini',
+    section => 'xdebug',
     require   => Package['php5-cli']
 }
 
@@ -58,20 +88,21 @@ class { 'apache':
 include apache::mod::rewrite
 include apache::mod::php
 
-apache::vhost { 'bamboo.dev':
-    port          => '80',
-    docroot       => '/home/vagrant/bamboo/web',
-    docroot_owner => 'vagrant',
-    docroot_group => 'vagrant',
-    directories   => [
-        {
-            path           => '/home/vagrant/bamboo/web',
-            options        => ['Indexes','FollowSymLinks','MultiViews'],
-            allow_override => ['all'],
-            allow => 'from All'
-        },
-    ],
-    serveradmin => 'admin@bamboo.com',
+apache::vhost { 
+	'bamboo.dev':
+    	port          => '80',
+    	docroot       => '/home/vagrant/bamboo/web',
+    	docroot_owner => 'vagrant',
+    	docroot_group => 'vagrant',
+    	directories   => [
+        	{
+			    path           => '/home/vagrant/bamboo/web',
+			    options        => ['Indexes','FollowSymLinks','MultiViews'],
+			    allow_override => ['all'],
+			    allow => 'from All'
+	        },
+    	],
+    	serveradmin => 'admin@bamboo.com',
 }
 
 class { 'mysql::server':
@@ -91,3 +122,4 @@ exec { 'bamboo_install':
     timeout     => 1800,
     require     => Exec['composer_config']
 }
+
